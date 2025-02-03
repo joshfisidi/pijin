ee57cdc5e167208046e17fe301c155770c885e36
joshfisidi <josh@fisidi.com>
2025-02-03 16:06:32 -0500

    setting up dynamic user routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -26,9 +26,9 @@ export default async function MainLayout({
 
   return (
     <div className="min-h-screen flex flex-col">
-      <main className="flex-1 container mx-auto px-4 py-8">
+      <div className="flex-1">
         {children}
-      </main>
+      </div>
 
       <nav className="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
         <div className="container mx-auto px-4">
